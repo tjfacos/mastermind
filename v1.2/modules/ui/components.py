0c4061bdd0243d89fd3d71cf3210912b0155e39c
Thomas Facos <tjfacos@gmail.com>
2023-01-05 10:12:53 +0000

    Added v1.2

---
+++ v1.2/modules/ui/components.py
@@ -0,0 +1,106 @@
+import pygame
+
+class Image: #Image class to use in labels, menus etc.
+    
+    def setImageRect(self):
+        
+        self.image = pygame.transform.scale(self.img, (
+            int(self._width), 
+            int(self._height)
+        ))
+
+        self.rect = self.image.get_rect()
+
+    @property
+    def topleft(self):
+        return self.rect.topleft
+    
+    @topleft.setter
+    def topleft(self, topleft):
+        self.rect.topleft = topleft
+
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self, h):
+        self._height = h
+        self.setImageRect()
+
+    @property
+    def width(self):
+        return self._width * self._scale
+    
+    @width.setter
+    def width(self, w):
+        self._width = w
+        self.setImageRect()
+
+
+
+
+    def __init__(self, x, y, width, height, img_path) -> None:
+        self.img = pygame.image.load(img_path).convert_alpha()
+        
+        
+        self._width = width
+        self._height = height
+
+        self.setImageRect()
+        
+        self.topleft = (x,y)
+    
+
+
+    def run(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+class Button(Image):
+    
+    def __init__(self, x, y, width, height, img_path, method) -> None:
+        
+        super().__init__(x, y, width, height, img_path)
+
+        self.method = method
+        self.clicked = False
+
+    def run(self, screen, args=()):
+        #Draw button
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        #Get mouse position
+        pos = pygame.mouse.get_pos()
+
+        #Check moused over and clicked
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] and not self.clicked:
+                self.clicked = True
+                return self.method(*args)
+        
+        if not pygame.mouse.get_pressed()[0]:
+            self.clicked = False
+
+class Label:
+       
+    def __init__(self, x, y, text, font_size=35, colour = (255,255,255)) -> None:
+        pygame.font.init()
+        self.font = pygame.font.SysFont("Ariel", font_size)
+        self.text = self.font.render(text, True, colour)
+        self.rect = self.text.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def run(self, screen):
+        screen.blit(self.text, self.rect)
+
+
+
+
+
+
+
+
+
---
+++ v1.2/modules/ui/menu.py
@@ -0,0 +1,94 @@
+from modules.ui.components  import *
+from modules.ui.config import Colours, Path
+from modules.ui.element_functions import exit_game
+
+import pygame
+
+class Win_Box:
+    def __init__(self, SCREEN_DIMS, topleft, replay_method) -> None:
+        self.width = SCREEN_DIMS.width - topleft[0]
+        self.height = SCREEN_DIMS.height
+
+        self.outer_rect = pygame.Rect(topleft[0], topleft[1], self.width, self.height)
+
+        self.state_image = Image(topleft[0], topleft[1] + 10, self.width, self.height * 0.5, Path("you_win") )
+        
+
+        btn_topleft = (topleft[0], topleft[1] + self.height * 0.5 + 10)
+        self.play_again_btn = Button(
+            btn_topleft[0], 
+            btn_topleft[1], 
+            self.width, 
+            self.height * 0.2,  
+            Path("replay"), 
+            replay_method
+        )
+    
+    def run(self, screen, replay_method):
+        
+        pygame.draw.rect(
+            screen,
+            Colours.LIGHT_GREY,
+            self.outer_rect,
+            0,
+            10
+        )
+
+        self.state_image.run(screen)
+        self.play_again_btn.run(screen)
+
+class Lose_Box(Win_Box):
+    def __init__(self, SCREEN_DIMS, topleft, replay_method) -> None:
+        super().__init__(SCREEN_DIMS, topleft, replay_method)
+        self.state_image = Image(topleft[0], topleft[1] + 10, self.width, self.height * 0.5, Path("you_lose") )
+
+
+
+
+
+
+
+
+
+class Menu():
+
+    def __init__(self, SCREEN_DIMS):
+        
+        self.background_rect = pygame.Rect(0, 0, SCREEN_DIMS.width, SCREEN_DIMS.height)
+        self.game_started = False
+        
+        self.logo = Image(0, 0, SCREEN_DIMS.width, SCREEN_DIMS.height * 0.4, Path("mastermind-logo"))
+        
+
+        #Define Button Width
+        btn_width = SCREEN_DIMS.width / 5
+        
+        #Define Start Button
+        self.start_btn = Button(0, 0, btn_width, btn_width*0.6, Path("start_btn"), self.start_game)
+        self.start_btn.topleft = (SCREEN_DIMS.width/2 - btn_width/2, self.logo.height + 50)
+        
+        #Define Quit Button
+        self.quit_btn = Button(0, 0, btn_width, btn_width*0.6, Path("stop_btn"), exit_game)
+        self.quit_btn.topleft = (SCREEN_DIMS.width/2 - btn_width/2, self.logo.height + 50 + self.start_btn.height + 50)
+
+    def start_game(self):
+        self.game_started = True
+
+    def run(self, screen):
+        pygame.draw.rect(screen, Colours.DARK_GREY, self.background_rect)
+
+        self.logo.run(screen)
+        self.start_btn.run(screen)
+        self.quit_btn.run(screen)
+
+
+
+class LeaderBoard():
+    pass
+
+
+
+
+
+
+
